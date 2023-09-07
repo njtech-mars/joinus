513967bb3d444cbd6d8463659b6ac95628795387
MR-Addict <MR-Addict@qq.com>
2023-09-08 00:10:42 +0800

    fix: fix unable to submit form

--- client/src/components/Modal/Modal.svelte
+++ client/src/components/Modal/Modal.svelte
@@ -12,23 +12,26 @@
 	}
 </script>
 
-{#if showModal}
-	<div class="backdrop">
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<div class="modal" on:click|self={() => (showModal = false)}>
-			<slot />
-		</div>
+<div class="backdrop" class:active={showModal}>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="modal" class:active={showModal} on:click|self={() => (showModal = false)}>
+		<slot />
 	</div>
-{/if}
+</div>
 
 <style lang="postcss">
 	.backdrop {
-		@apply bg-black/30 fixed inset-0;
+		@apply bg-black/30 fixed hidden inset-0;
+		&.active {
+			@apply block;
+		}
 	}
 	.modal {
 		@apply w-full h-full flex flex-col items-center justify-center;
-		animation: zoom 500ms cubic-bezier(0.34, 1.56, 0.64, 1);
+		&.active {
+			animation: zoom 500ms cubic-bezier(0.34, 1.56, 0.64, 1);
+		}
 	}
 	@keyframes zoom {
 		from {
